dc698ca39a9fe88fae1935147aae355e59d3b304
Attawut Arpornrat <60070116@kmitl.ac.th>
2020-05-08 21:14:56 +0700

    Project 1

--- Proj-1(AI)/Assets/Scripts/DetectCollisions.cs
+++ Proj-1(AI)/Assets/Scripts/DetectCollisions.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DetectCollisions : MonoBehaviour
+
+
 {
     private void OnTriggerEnter(Collider other)
-    {   
+    {
         Destroy(gameObject);
-        Destroy(other.gameObject);        
+        Destroy(other.gameObject);
+
+        Score.scoreValue += 0.5;
+
         if (other.gameObject.name == "Player")
         {
-            GameObject.Find("EnemySpawner").SendMessage("GameOver");            
+            GameObject.Find("EnemySpawner").SendMessage("GameOver");
         }
     }
 }
